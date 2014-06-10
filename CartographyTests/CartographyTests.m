7a708bc8989dce9d28ef2c3dafd6edd685db15cd
David Groulx <david@sandbendersoftware.com>
2014-06-10 14:22:36 -0400

    Initial Commit

---
+++ CartographyTests/CartographyTests.m
@@ -0,0 +1,34 @@
+//
+//  CartographyTests.m
+//  CartographyTests
+//
+//  Created by David Groulx on 6/10/14.
+//  Copyright (c) 2014 David Groulx. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CartographyTests : XCTestCase
+
+@end
+
+@implementation CartographyTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
